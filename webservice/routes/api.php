7ebd485c72663bb46a28c291a736ac17648d0972
silasgon <silasidc@gmail.com>
2019-10-18 11:59:21 -0300

    implementado token do user.

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -24,10 +24,11 @@ Route::post('/cadastro', function(Request $request){
         'email' => $data['email'],
         'password' => bcrypt($data['password']),
     ]);
+    $user->token = $user->createToken($user->email)->accessToken;
 
     return $user;
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
