46ae5c1c1481c4bbe9728e8785237aa43b4ffb24
silasgon <silasidc@gmail.com>
2019-10-18 18:06:19 -0300

    aplicando validação na api.

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,17 @@ Route::get('/teste', function(Request $request){
 });
 Route::post('/cadastro', function(Request $request){
     $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+        
+    if($validacao->fails()){
+        return $validacao->errors();
+
+    }
     $user = User::create([
         'name' => $data['name'],
         'email' => $data['email'],
